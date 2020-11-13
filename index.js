91601b6fc3a7756be700186ba5c70dc8f8a4cc5a
Richard Leung <michelleli@MMac.local>
2020-11-13 17:01:07 -0500

    ejs

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs')
+
 app.get('/', function(req, res) {
-    res.send('Hello, SEI World');
+    res.render('index', {myVAr: 'woohoo'});
 });
 
 app.get('/about', function(req, res) {
-    res.send('This is the about page');
+    res.render('about');
 });
 
 app.get('/blog', function(req, res) {
-    res.send('Welcome to my blog');
+    res.render('blog-generic.ejs');
 });
 
-app.listen(3000);
+app.get('/blog/:date', function(req, res) {
+    res.render('blog', { date: req.params.date });
+});
+
+app.listen(8000, () => {
+    console.log('server started!');
+});
